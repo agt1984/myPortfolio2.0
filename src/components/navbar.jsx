314048ae76501922298dd9adf99a4d2a1453c435
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-04-08 23:06:39 +0200

    menu added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -13,8 +13,13 @@ const Navbar = () => {
     const [open, setOpen] = useState(false)
     return (
       <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-        {/*LOGO*/}
-        <div className="">
+        <div className="hidden">
+          {links.map(link =>(
+            <Link href={link.url} key={link.title}>{link.title}</Link>
+          ))}
+        </div>
+        {/* LOGO */}
+        <div className="md:hidden">
           <Link
             href="/"
             className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
@@ -25,27 +30,28 @@ const Navbar = () => {
             </spam>
           </Link>
         </div>
-        {/*Menu RESPNSIVE */}
-        <div className="">
-          {/*Menu button */}
-          <button className="w-10 h-8 flex flex-col justify-between z-50 relative">
+        {/* Menu RESPNSIVE */}
+        <div className="md:hidden">
+          {/* Menu button */}
+          <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={(()=>setOpen((prev) => !prev))}>
             <div className="w-10 h-1 bg-white rounded"></div>
             <div className="w-10 h-1 bg-white rounded"></div>
             <div className="w-10 h-1 bg-white rounded"></div>
           </button>
           {/*MENU LIST*/}
-          {
-          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
-            {links.map((link) => (
-              <Link href={link.url} key={link.title}>
-                {link.title}
-              </Link>
-            ))}
-          </div>
-          }
+          {open && (
+
+            <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+              {links.map((link) => (
+                <Link href={link.url} key={link.title}>
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     );
-}
+};
  
 export default Navbar;
