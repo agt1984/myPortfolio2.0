bbc612e123c621b9732d23c8b04eaffdef75250c
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-04-09 09:59:55 +0200

    social networks added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,8 @@
 "use client"
 import Link from "next/link";
 import { useState } from "react";
+import Image from "next/image";  //en el tutorial no aparece, pero, aqui de debe usar para colocar las imagenes
+
 
 const links = [
   { url: "/", title: "Home" },
@@ -13,9 +15,11 @@ const Navbar = () => {
     const [open, setOpen] = useState(false)
     return (
       <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-        <div className="hidden">
-          {links.map(link =>(
-            <Link href={link.url} key={link.title}>{link.title}</Link>
+        <div className="hidden md:flex gap-4">
+          {links.map((link) => (
+            <Link href={link.url} key={link.title}>
+              {link.title}
+            </Link>
           ))}
         </div>
         {/* LOGO */}
@@ -30,17 +34,29 @@ const Navbar = () => {
             </spam>
           </Link>
         </div>
+
+        <div className="">
+          <Link href="https://github.com/agt1984">
+            <Image src="/github.png" alt="" width={24} height={24} />
+          </Link>
+          <Link href="https://www.linkedin.com/in/alvaro-luis-gonzalez-trejo-93a421125/">
+            <Image src="/linkedin.png" alt="" width={24} height={24} />
+          </Link>
+        </div>
+
         {/* Menu RESPNSIVE */}
         <div className="md:hidden">
           {/* Menu button */}
-          <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={(()=>setOpen((prev) => !prev))}>
+          <button
+            className="w-10 h-8 flex flex-col justify-between z-50 relative"
+            onClick={() => setOpen((prev) => !prev)}
+          >
             <div className="w-10 h-1 bg-white rounded"></div>
             <div className="w-10 h-1 bg-white rounded"></div>
             <div className="w-10 h-1 bg-white rounded"></div>
           </button>
           {/*MENU LIST*/}
           {open && (
-
             <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
               {links.map((link) => (
                 <Link href={link.url} key={link.title}>
