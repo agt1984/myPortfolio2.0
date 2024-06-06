a80bed33ef35d2831fe027679f636b8366ba286e
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-06-06 23:52:56 +0200

    nav component hamburger menu added

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,17 @@
 import { navMenuList } from "@/data/navigationData";
 import { CustomLink } from "./CustomLink";
+import { FiMenu } from "react-icons/fi";
+import { cn } from "@/utils/helper";
 
 export function Navigation() {
     return (
       <nav className="py-4 border-b border-zinc-800 shadow-md sticky top-0 z-10 bg-zinc-900/20 backdrop-blur-md">
         <div className="max-w-7xl mx-auto px-4 md:px-8 flex justify-between items-center">
           <p>logo</p>
-          <div>
+          <div className="md:hidden">
+            <FiMenu className="text-2xl"/>
+          </div>
+          <div className={cn("max-md:fixed max-md:top-0 max-md:py-10 max-md:left-0 max-md:w-full max-md:min-h-screen max-md:bg-red-400")}>
             <ul className="flex items-center gap-6 ">
               {navMenuList.map(({ title, href }, index) => {
                 const isLastElement = index === navMenuList.length - 1;
