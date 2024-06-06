3a6ecb8927c443506799dd301dc574f596554157
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-06-06 23:39:47 +0200

    nav component modified

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,10 +4,10 @@ import { CustomLink } from "./CustomLink";
 export function Navigation() {
     return (
       <nav className="py-4 border-b border-zinc-800 shadow-md sticky top-0 z-10 bg-zinc-900/20 backdrop-blur-md">
-        <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <div className="max-w-7xl mx-auto px-4 md:px-8 flex justify-between items-center">
           <p>logo</p>
           <div>
-            <ul>
+            <ul className="flex items-center gap-6 ">
               {navMenuList.map(({ title, href }, index) => {
                 const isLastElement = index === navMenuList.length - 1;
                 return (
