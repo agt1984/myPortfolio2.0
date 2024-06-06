649ac4372e4f094058e91b7371f436775673ff5e
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-06-06 23:58:14 +0200

    nav component hamburger menu added

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -11,8 +11,8 @@ export function Navigation() {
           <div className="md:hidden">
             <FiMenu className="text-2xl"/>
           </div>
-          <div className={cn("max-md:fixed max-md:top-0 max-md:py-10 max-md:left-0 max-md:w-full max-md:min-h-screen max-md:bg-red-400")}>
-            <ul className="flex items-center gap-6 ">
+          <div className={cn("max-md:fixed max-md:top-0 max-md:py-10 max-md:left-0 max-md:w-full max-md:min-h-screen max-md:bg-zinc-900/90 max-md:black-blur-lg")}>
+            <ul className="flex items-center flex-col md:flex-row gap-6 ">
               {navMenuList.map(({ title, href }, index) => {
                 const isLastElement = index === navMenuList.length - 1;
                 return (
