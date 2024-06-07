db01a00a12f3d9858d4a7286eb807892b673dd6a
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-06-07 16:58:11 +0200

    hamburguer menu modified

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -11,7 +11,11 @@ export function Navigation() {
           <div className="md:hidden">
             <FiMenu className="text-2xl"/>
           </div>
-          <div className={cn("max-md:fixed max-md:top-0 max-md:py-10 max-md:left-0 max-md:w-full max-md:min-h-screen max-md:bg-zinc-900/90 max-md:black-blur-lg")}>
+          <div 
+            className={cn(
+              "max-md:fixed max-md:top-[58px] max-md:py-10 max-md:left-0 max-md:w-full max-md:min-h-screen          max-md:bg-zinc-900/90 max-md:black-blur-lg"
+            )}
+          >
             <ul className="flex items-center flex-col md:flex-row gap-6 ">
               {navMenuList.map(({ title, href }, index) => {
                 const isLastElement = index === navMenuList.length - 1;
