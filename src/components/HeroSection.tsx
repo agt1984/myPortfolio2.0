aec407d797bab1bf0af25b6f00d8ce4368924324
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-06-07 17:37:32 +0200

    HeroSection component added

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,3 +1,17 @@
+import { CustomLink } from "./CustomLink";
+
 export function HeroSection() {
-    return <section className="grid items-center h-[calc(100vh)]"></section>;
+    return (  
+        <section className="grid md:grid-cols-2 items-center h-[calc(100vh-70px)] max-h-[678px]">
+            <div>
+                <p>Hello There</p>
+                <h1>Full Stack Developer</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dolores temporibus omnis reiciendis atque et nobis? Voluptatibus, officia quibusdam consectetur hic dolores dolore recusandae nemo nam blanditiis deserunt corrupti! Itaque.
+                </p>
+                <CustomLink href="#project">My Work</CustomLink>
+            </div>
+            <div></div>
+        </section>
+    );
 }
