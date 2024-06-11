4894032ddc7220decf4b09602c2b5bc9bc551ae8
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-06-11 22:20:56 +0200

    Project Section Modified

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,8 +1,44 @@
+import { projects } from "@/data/projectData";
+import Image from "next/image";
+
 export function ProjectSection() {
-    return (
-        <section>
-            <p className="font-title tracking-wider text-4xl uppercase">Project</p>
-            <p className="text-zinc-400">my best project i have built</p>
-        </section>
-    );
+  return (
+    <section>
+      <p className="font-title tracking-wider text-4xl uppercase">Project</p>
+      <p className="text-zinc-400">My best project i have built</p>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {projects.map((project) => {
+          return <ProjectCard key={project.title} project={project}/>;
+        })}
+      </div>
+    </section>
+  );
+}
+
+interface IProjectCard {
+  project: (typeof projects)[0];
+}
+
+function ProjectCard({ project }: IProjectCard) {
+  return (
+    <div className="bg-zinc-800/50 border border-zinc-800 rounded-lg overflow-hidden">
+      <div>
+        <Image
+          src={project.image}
+          alt={project.title}
+          width={400}
+          height={400}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="p-4">
+        <ul>
+          {project.stacks.map((stack) => {
+            return <li key={stack}>{stack}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
---
+++ src/data/projectData.ts
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    title: "Chat-bot-react",
+    desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit",
+    stacks: ["Next.Js", "Express", "Socket.10"],
+    image: "/agt.jpg",
+    sourceCode: "https://github.com/",
+    liveAt: "https://github.com/",
+  },
+  {
+    title: "Chat-bot-react",
+    desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit",
+    stacks: ["Next.Js", "Express", "Socket.10"],
+    image: "/agt.jpg",
+    sourceCode: "https://github.com/",
+    liveAt: "https://github.com/",
+  },
+  {
+    title: "Chat-bot-react",
+    desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit",
+    stacks: ["Next.Js", "Express", "Socket.10"],
+    image: "/agt.jpg",
+    sourceCode: "https://github.com/",
+    liveAt: "https://github.com/",
+  },
+  {
+    title: "Chat-bot-react",
+    desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et, consectetur adipiscing elit",
+    stacks: ["Next.Js", "Express", "Socket.10"],
+    image: "/agt.jpg",
+    sourceCode: "https://github.com/",
+    liveAt: "https://github.com/",
+  },
+];
