c6fc330d330617995627f86a14b01d705851ac99
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-06-11 14:34:57 +0200

    Project Section Created

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,7 +1,8 @@
 export function ProjectSection() {
     return (
         <section>
-            <p>Project</p>
+            <p className="font-title tracking-wider text-4xl uppercase">Project</p>
+            <p className="text-zinc-400">my best project i have built</p>
         </section>
     );
 }
