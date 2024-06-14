ff118fc62cf4bfb1ab622fcb1d70a8cbccb48ac0
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-06-14 18:20:00 +0200

    project section modified

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,10 +1,11 @@
 import { projects } from "@/data/projectData";
 import Image from "next/image";
+import { CustomLink } from "./CustomLink";
 
 export function ProjectSection() {
   return (
     <section>
-      <p className="font-title tracking-wider text-4xl uppercase">Project</p>
+      <p className="font-title tracking-wider text-4xl uppercase">Projects</p>
       <p className="text-zinc-400">My best project i have built</p>
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -36,15 +37,21 @@ function ProjectCard({ project }: IProjectCard) {
         <ul className="flex gap-4 mb-2">
           {project.stacks.map((stack) => {
             return (
-              <li 
-                  key={stack} 
-                  className="px-4 py-1 text-xs border border-r-zinc-700 text-zinc-400 rounded-md"
+              <li
+                key={stack}
+                className="px-4 py-1 text-xs border border-zinc-700 text-zinc-400 rounded-md"
               >
                 {stack}
               </li>
             );
           })}
         </ul>
+        <p>{project.title}</p>
+        <p className="text-zinc-400">{project.desc}</p>
+        <div className="flex gap-4 mt-4">
+          <CustomLink href={project.sourceCode}>Source Code</CustomLink>
+          <CustomLink href={project.liveAt}>Source Code</CustomLink>
+        </div>
       </div>
     </div>
   );
