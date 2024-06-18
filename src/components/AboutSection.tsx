3baa7f2a946c1efb7f13cb6a0c80f8e0c93b86e7
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-06-18 12:33:01 +0200

    About section created

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
 import { Heading } from "./Heading";
 
 export function AboutSection() {
     return (
         <section>
-            <Heading title="About me" desc=""/>
+            <Heading title="About me" desc="Let me know something about me"/>
+
+            <div className="flex gap-4 flex-col md:flex-row items-center">
+                <div className="h-80 w-80 shrink-0 rounded-md overflow-hidden">
+                    <Image src="/agt.jpg" alt="" width={600} height={600} className="w-full h-full object-top object-cover"/>
+                </div>
+            </div>
         </section>
     );
 }
