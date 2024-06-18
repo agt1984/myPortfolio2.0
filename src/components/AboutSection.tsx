f3fd9e82e47c1d089f3783795ceaec311872d6cf
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-06-18 10:57:36 +0200

    About section created

---
+++ src/components/AboutSection.tsx
@@ -0,0 +1,9 @@
+import { Heading } from "./Heading";
+
+export function AboutSection() {
+    return (
+        <section>
+            <Heading title="About me" desc=""/>
+        </section>
+    );
+}
---
+++ src/data/socialLinksData.ts
@@ -0,0 +1,20 @@
+import {
+    AiOutlineInstagram,
+    AiOutlineLinkedin,
+    AiOutlineGithub
+} from "react-icons/ai";
+
+export const socialLinks = [
+  {
+    href: "instagram.com",
+    icon: AiOutlineInstagram,
+  },
+  {
+    href: "instagram.com",
+    icon: AiOutlineLinkedin,
+  },
+  {
+    href: "instagram.com",
+    icon: AiOutlineGithub,
+  },
+];
