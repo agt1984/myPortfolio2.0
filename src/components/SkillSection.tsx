5c348d4564d00548016e63c8c7bc6ceaa990df43
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-06-18 14:35:34 +0200

    About section MODIFIED

--- src/components/SkillSection.tsx
+++ src/components/SkillSection.tsx
@@ -11,7 +11,7 @@ export function SkillsSection() {
                 return (
                     <div key={cate.title}>
                         <p className="mb-2">{cate.title}</p>
-                        <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
+                        <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 mb-6">
                             {cate.stacks.map(({ icon: Icon, stack }) => {
                                 return (
                                     <li key={stack} className="bg-zinc-800/50 border border-zinc-800 p-4 inline-flex items-center gap-4 justify-center rounded-md">
